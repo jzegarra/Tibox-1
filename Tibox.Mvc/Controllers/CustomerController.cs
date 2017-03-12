b91617243e900991118c2e500e781e2088e79aee
juvega <cevegaju@hotmail.com>
2017-03-11 23:45:45 -0500

    Configuración inicial MVC

---
+++ Tibox.Mvc/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tibox.Models;
+using Tibox.UnitOfWork;
+
+namespace Tibox.Mvc.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly TiboxUnitOfWork _unit;
+        public CustomerController()
+        {
+            _unit = new TiboxUnitOfWork();
+        }
+        public ActionResult Index()
+        {
+            return View(_unit.Customers.GetAll());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                var id = _unit.Customers.Insert(customer);
+                if (id > 0) return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
