a1fa8acafff835c5b63574c135125526a6915bcb
juvega <cevegaju@hotmail.com>
2017-03-11 20:40:48 -0500

    Javascript first approach.

---
+++ Tibox.Html5/scripts/index.js
@@ -0,0 +1,12 @@
+﻿var tibox = tibox || {};
+
+(function () {
+    tibox.Index = {
+        currentYear: function () {
+            var today = new Date();
+            return today.getFullYear();
+        }
+    };
+
+    document.getElementById("date").innerHTML = tibox.Index.currentYear();
+})();
