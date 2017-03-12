5a1d0b537ee295650614e063708a140b82c82405
juvega <cevegaju@hotmail.com>
2017-03-11 22:34:01 -0500

    Javascript con IIFE.

--- Tibox.Html5/scripts/index.js
+++ Tibox.Html5/scripts/index.js
@@ -1,12 +1,9 @@
-﻿var tibox = tibox || {};
-
-(function () {
+﻿(function (tibox) {
     tibox.Index = {
         currentYear: function () {
             var today = new Date();
             return today.getFullYear();
         }
     };
-
     document.getElementById("date").innerHTML = tibox.Index.currentYear();
-})();
+})(window.tibox = window.tibox || {});
